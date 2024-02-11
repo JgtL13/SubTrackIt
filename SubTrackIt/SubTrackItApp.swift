e4a5e085ca83180b7bba9d229d21c48760a8d0ce
JgtL13 <jlin88@tamu.edu>
2024-02-11 00:45:29 -0600

    Initial Commit

---
+++ SubTrackIt/SubTrackItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubTrackItApp.swift
+//  SubTrackIt
+//
+//  Created by Justin Lin on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SubTrackItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
