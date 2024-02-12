133891474925275bcdead7993ffca22881068f6f
JgtL13 <jlin88@tamu.edu>
2024-02-11 20:55:09 -0600

    Added Firebase dependencies and .gitignore

--- SubTrackIt/SubTrackItApp.swift
+++ SubTrackIt/SubTrackItApp.swift
@@ -5,10 +5,14 @@
 //  Created by Justin Lin on 2/11/24.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct SubTrackItApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
